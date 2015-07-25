3f9a0b4b8afd06e48437fd7ac780bcbf8738a1f8
shonenada <shonenada@gmail.com>
2015-07-25 21:33:58 +0800

    add flask_bearychat/__init__.py

---
+++ flask_bearychat/__init__.py
@@ -0,0 +1,12 @@
+"""
+    flask_bearychat
+    ~~~~~~~~~~~~~~~
+
+    A Flask extension to help interact with BearyChat.
+
+    :copyright: (c) 2015 Yaoda Liu.
+    :license: MIT
+"""
+
+__all__ = ['']
+__version__ = '0.1.0'
